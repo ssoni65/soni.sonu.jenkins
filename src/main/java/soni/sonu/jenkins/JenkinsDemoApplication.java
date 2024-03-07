1913afb11a20a343bfc149714bec408a64e0cc7f
Sonu Soni <kssoni65@outlook.com>
2024-03-07 23:00:45 +0530

    Updated pom.xml

--- src/main/java/soni/sonu/jenkins/JenkinsDemoApplication.java
+++ src/main/java/soni/sonu/jenkins/JenkinsDemoApplication.java
@@ -1,11 +1,12 @@
 package soni.sonu.jenkins;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class JenkinsDemoApplication {
